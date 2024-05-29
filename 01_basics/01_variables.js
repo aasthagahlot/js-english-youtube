936eb80b6c94f04b96d4c992a22d504a9d9d5f3a
aasthagahlot <84974318+aasthagahlot@users.noreply.github.com>
2024-05-29 07:10:07 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "aastha@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+//accountId = 2//  not allowed
+
+accountEmail = "has@gmail.com"
+accountPassword = "12367"
+accountCity = "bengluru"
+let accountState ; //if we didn't assign any value then it will give output = undefined//
+
+console.log(accountId);
+
+/*
+perfer not to use var
+because of isssue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState])
