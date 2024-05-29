e2f4b992db2da466ddaa90f58d3fd7a7aed4fae0
aasthagahlot <84974318+aasthagahlot@users.noreply.github.com>
2024-05-29 08:08:11 +0000

    commit

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -25,3 +25,45 @@ let age = 23
 let stringNumber = String(age)
 console.log(stringNumber)
 console.log(typeof(stringNumber))
+
+
+
+
+//   OPERATIONS  //
+
+let value = 3;
+let negval = -value;
+console.log(negval)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**3)
+console.log(2%2)
+console.log(2/2)
+
+
+// strings addition
+
+let str1 = "Hello, Myself ";
+let str2 = "Aastha";
+
+let str3 = str1 + str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// prefix and postfix
+
+let gamecounter = 100;
+gamecounter++; //postfix
+console.log(gamecounter);
+
+let game = 101;
+++game;  //prefix
+console.log(game);
