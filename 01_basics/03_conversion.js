7dd2ac43aa74ca934a44caeba2831965efe86750
aasthagahlot <84974318+aasthagahlot@users.noreply.github.com>
2024-05-29 07:46:05 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,27 @@
+let score = "33"
+let scores = null
+let scoress = undefined
+let scoree = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof(valueInNumber));
+
+// "33" => 33
+//  "33abc" => NaN
+// true => 1 , false => 0
+
+// "" => false
+// "hitesh" => true
+
+
+
+// string to number conversions
+let age = 23
+
+let stringNumber = String(age)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
