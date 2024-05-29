acf6b770fb9c47199e5f4a9d2d9150996c6b0661
aasthagahlot <84974318+aasthagahlot@users.noreply.github.com>
2024-05-29 07:24:09 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer versions
+
+// alert(3+3)  we are using nodejs not browser
+
+let name = "Aastha"
+let age = 23
+let isLoggedIn = false
+
+//number => 2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assign yet
+// symbol => unique
+
+
+//object 
+
+console.log(typeof age);  //used to find the type of datatype
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null);      //object
