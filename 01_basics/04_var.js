bb32a84be812954abd74fd277391bcaca08e2f33
aasthagahlot <84974318+aasthagahlot@users.noreply.github.com>
2024-07-05 12:31:27 +0530

    go

--- 01_basics/04_var.js
+++ 01_basics/04_var.js
@@ -1,1 +1,13 @@
-aastha
+const accountId = 144553
+let accountEmail = "aastha@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+
+//accountId = 2//  not allowed
+
+accountEmail = "has@gmail.com"
+accountPassword = "12367"
+accountCity = "bengluru"
+let accountState ; //if we didn't assign any value then it will give output = undefined//
+
+console.log(accountId);
